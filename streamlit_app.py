3091a2f3ece223c49f7e42977ec13e38b5aa73b7
Thibault Lanthiez <thibault.lanthiez@outlook.fr>
2022-06-04 00:53:32 +0200

    correct mobile issue

--- streamlit_app.py
+++ streamlit_app.py
@@ -23,7 +23,6 @@ translation = english_translator.translate(today_date, dest="fr")
 col1, col2 = st.columns(2)
 st.title("Ce soir à la télé")
 st.title(translation.text)
-st.title(platform.uname().system)
 
 progress_bar = st.progress(0)
 
@@ -214,11 +213,14 @@ for i, index_movie in enumerate(movie_ranking):
     if press_rate == 0:
         press_rate = "aucune note"
 
-    for index_col, col in enumerate(list_col_cinema):
-        if i in range(index_col, 20, nb_col):
-            column = col
-            break
-
+    if platform.uname().system == "Linux":
+        column = st
+    else:
+        for index_col, col in enumerate(list_col_cinema):
+            if i in range(index_col, 20, nb_col):
+                column = col
+                break
+        
     column.write("_____")
     column.image(img_movie_allocine, width=120)
     column.markdown(f"{channel} (chaîne {channel_number[2:]}) - {starting_hour}")
@@ -239,10 +241,13 @@ def show_prog(title, data):
     for i, prog in enumerate(data):
         img_movie, channel, channel_number, starting_hour, title, subtitle = prog
         
-        for index_col, col in enumerate(list_col):
-            if i in range(index_col, 20, nb_col):
-                column = col
-                break
+        if platform.uname().system == "Linux":
+            column = st
+        else:
+            for index_col, col in enumerate(list_col_cinema):
+                if i in range(index_col, 20, nb_col):
+                    column = col
+                    break
 
         column.write("_____")
         column.image(img_movie, width=120)
